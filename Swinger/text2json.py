71cdf2167d7222c92bfc808e1b85a6b077df0296
david <davidtnfsh@gmail.com>
2017-04-21 13:50:30 +0800

    把text2json轉成spark版本

--- Swinger/text2json.py
+++ Swinger/text2json.py
@@ -1,14 +1,13 @@
+import pyspark
 import jieba, json, sys
+conf = pyspark.SparkConf().setAll([('spark.driver.memory', '30g'), ('spark.driver.host', '172.17.0.21'), ('spark.app.id', 'local-1492693477461'), ('spark.rdd.compress', 'True'), ('spark.serializer.objectStreamReset', '100'), ('spark.master', 'local[*]'), ('spark.executor.id', 'driver'), ('spark.submit.deployMode', 'client'), ('spark.driver.port', '39274'), ('spark.app.name', 'PySparkShell')])
+sc = pyspark.SparkContext(conf=conf)
 stopwords = json.load(open('stopwords.json', 'r'))
-def removeStopWords(jiebaGen):
-	return list(filter(lambda x: x not in stopwords, jieba.cut(jiebaGen)))
-with open(sys.argv[1], 'r') as f:
-	result = list(map(removeStopWords, f))
-	ff = open('p.json', 'w')
-	json.dump(result, ff)
-	ff.close()
-with open(sys.argv[2], 'r') as f:
-	result = list(map(removeStopWords, f))
-	ff = open('n.json', 'w')
-	json.dump(result, ff)
-	ff.close()
+def removeStopWords(sentence):
+	return list(filter(lambda x: x not in stopwords, jieba.cut(sentence)))
+
+t = sc.textFile(sys.argv[1])
+result = t.map(removeStopWords).collect()
+ff = open(sys.argv[2], 'w')
+json.dump(result, ff)
+ff.close()
